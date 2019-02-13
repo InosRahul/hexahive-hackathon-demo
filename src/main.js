b30aaae08a71ad490ff70a12e5f41657fe7056ee
InosRahul <rahulsoni2225@gmail.com>
2019-02-13 12:33:32 +0530

    final commit

--- src/main.js
+++ src/main.js
@@ -1,11 +1,19 @@
-import { ApolloClient } from 'apollo-client'
-import { HttpLink } from 'apollo-link-http'
-import { InMemoryCache } from 'apollo-cache-inmemory'
-import { setContext } from 'apollo-link-context'
+import {
+  ApolloClient
+} from 'apollo-client'
+import {
+  HttpLink
+} from 'apollo-link-http'
+import {
+  InMemoryCache
+} from 'apollo-cache-inmemory'
+import {
+  setContext
+} from 'apollo-link-context'
 import VueApollo from 'vue-apollo'
 import Vue from 'vue'
 import App from './App.vue'
-import firebase from 'firebase' 
+import firebase from 'firebase'
 import router from './router'
 import './registerServiceWorker'
 
@@ -13,18 +21,20 @@ Vue.config.productionTip = false
 
 const config = {
   apiKey: "AIzaSyC-dsA3cwaYTz0i02NOkfLmDuXtmkZtQ9I",
-    authDomain: "hexa-hive-project.firebaseapp.com",
-    databaseURL: "https://hexa-hive-project.firebaseio.com",
-    projectId: "hexa-hive-project",
-    storageBucket: "hexa-hive-project.appspot.com",
-    messagingSenderId: "177744498688"
+  authDomain: "hexa-hive-project.firebaseapp.com",
+  databaseURL: "https://hexa-hive-project.firebaseio.com",
+  projectId: "hexa-hive-project",
+  storageBucket: "hexa-hive-project.appspot.com",
+  messagingSenderId: "177744498688"
 };
 firebase.initializeApp(config);
 const httpLink = new HttpLink({
   // URL to graphql server, you should use an absolute URL here
-  uri: 'http://demo-hackathon-hexa.herokuapp.com/v1alpha1/graphql'
+  uri: 'https://demo-hackathon-hexa.herokuapp.com/v1alpha1/graphql'
 })
-const authLink = setContext((_, { headers }) => {
+const authLink = setContext((_, {
+  headers
+}) => {
   // get the authentication token from localstorage if it exists
   const token = localStorage.getItem('attendance-app-token')
 
